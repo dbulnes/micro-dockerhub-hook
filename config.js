72a832477e423c0168b02929853e852563cc9298
david bulnes <bulnes.david@gmail.com>
2018-02-10 02:01:31 +0000

    adapting for multiple branch

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
   debug: process.env.DEBUG || true, // true: enable debug | any other: disable debug
-  token: process.env.TOKEN || 'abc123' // your secret token
+  token: process.env.TOKEN || '92b298eb-386d-4308-9b91-a9c52d959d27' // your secret token
 }
